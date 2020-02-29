ce868f96c6f442bde15d05aa008120365a93e8ed
Ryan <gal17027@byui.edu>
2020-02-29 02:28:09 -0700

    Added Button intents to navigate between login and create account layouts.

--- app/src/main/java/com/example/studybuddy/MainActivity.java
+++ app/src/main/java/com/example/studybuddy/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.studybuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //use create account button to open createActivity
+        Button createAccountButton = (Button) findViewById(R.id.createAccountMain);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent cIntent = new Intent(v.getContext(), createActivity.class);
+                startActivity(cIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/studybuddy/createActivity.java
+++ app/src/main/java/com/example/studybuddy/createActivity.java
@@ -1,7 +1,11 @@
 package com.example.studybuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class createActivity extends AppCompatActivity{
 
@@ -9,5 +13,14 @@ public class createActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        //use back button to go back to login page
+        Button createAccountButton = (Button) findViewById(R.id.goBackCreateButton);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent backIntent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(backIntent);
+            }
+        });
     }
 }
