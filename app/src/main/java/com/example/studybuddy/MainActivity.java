c5c3a50f5fb60cbb34b9facb42aba837599055cf
Ryan <gal17027@byui.edu>
2020-02-29 12:28:46 -0700

    Added some login functionality to login screen.

--- app/src/main/java/com/example/studybuddy/MainActivity.java
+++ app/src/main/java/com/example/studybuddy/MainActivity.java
@@ -4,10 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    //variables for login
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,35 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent cIntent = new Intent(v.getContext(), createActivity.class);
                 startActivity(cIntent);
+            }
+        });
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        inputEmail = (EditText) findViewById(R.id.editText);
+        inputPassword = (EditText) findViewById(R.id.passMain);
+        btnLogin = (Button) findViewById(R.id.mainLoginButton);
+
+        //login button functionality
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
+
+                if(TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //authenticate user
+                //MAKE NEW ACTIVITY AND LAYOUT FIRST!!
             }
         });
     }
