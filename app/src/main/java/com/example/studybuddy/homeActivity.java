6007271a6b0f47a1eba9212e90d882fff0184bbb
Ryan <gal17027@byui.edu>
2020-02-29 16:10:18 -0700

    Added functionality to log out and return to main screen.

--- app/src/main/java/com/example/studybuddy/homeActivity.java
+++ app/src/main/java/com/example/studybuddy/homeActivity.java
@@ -1,6 +1,9 @@
 package com.example.studybuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,15 @@ public class homeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //use sign out button to go back to login page
+        final Button createAccountButton = (Button) findViewById(R.id.button5);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent backIntent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(backIntent);
+            }
+        });
+
     }
 }
